14b035760f276885fdbda8c9f7e4db11394c6e58
Wilbert Alberts <wilbert.alberts@gmail.com>
2016-06-25 12:38:38 +0200

    Fix: remove 'eclipse-less' source files for Pi.

--- Raspberry/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-g++ -g -c -o rv_reg.o rv_reg.cpp
-g++ -g -c -o rv.o rv.cpp
-g++ -g -c -o main.o main.cpp
-g++ -g -c -o rv_loop.o rv_loop.cpp
-
-g++ -g -o rover rv.o main.o rv_loop.o rv_reg.o -lpthread
--- Raspberry/rv_loop.h
+++
@@ -1,7 +0,0 @@
-#ifndef RV_LOOP_H
-#define RV_LOOP_H
-
-extern void RV_startLoop();
-extern void RV_stopLoop();
-
-#endif
