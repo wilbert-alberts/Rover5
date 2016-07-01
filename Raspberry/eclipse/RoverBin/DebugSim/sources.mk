ea8b443706ad630b5e488b7d9b9c2da99f1cb0d5
Wilbert Alberts <wilbert.alberts@gmail.com>
2016-07-01 21:50:25 +0200

    Fix: add build environment
    	By checking in make files, alternative build 
    	environments can be used.

---
+++ Raspberry/eclipse/RoverBin/DebugSim/sources.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+INO_SRCS := 
+ASM_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+ELF_SRCS := 
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+PDE_SRCS := 
+CC_SRCS := 
+C_SRCS := 
+C_UPPER_DEPS := 
+SECONDARY_SIZE := 
+PDE_DEPS := 
+C_DEPS := 
+CC_DEPS := 
+C++_DEPS := 
+OBJS := 
+CXX_DEPS := 
+SECONDARY_FLASH := 
+ASM_DEPS := 
+INO_DEPS := 
+S_UPPER_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src \
+
