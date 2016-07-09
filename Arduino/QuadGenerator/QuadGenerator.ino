02374ceed83811ad9fed510cac2af8adf2fc94d8
Wilbert Alberts <wilbert.alberts@gmail.com>
2016-07-09 21:17:59 +0200

    Fix: quadrature generator now uses same ports as Rover.

--- Arduino/QuadGenerator/QuadGenerator.ino
+++ Arduino/QuadGenerator/QuadGenerator.ino
@@ -1,26 +1,48 @@
 
-#define PA (5)
-#define PB (6)
+#define PA (18)
+#define PB (19)
 
-#define TOTAL (1000)
+#define LED (13)
 
+int d= 0;
+
+#define TOTAL (5000UL)
+
+void waitPeriod(long  usecs);
 
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
+  pinMode(PA, OUTPUT);
+  pinMode(PB, OUTPUT);
+  pinMode(LED, OUTPUT);
 
+  Serial.println("Setup finished.");
 }
 
 void loop() {
+  d = 1 - d;
+  Serial.println(d);
+  digitalWrite(LED, d);
   
   // put your main code here, to run repeatedly:
   digitalWrite(PA, HIGH);
-  delayMicroseconds(TOTAL/4);
+  waitPeriod(TOTAL/4);
   digitalWrite(PB, HIGH);
-  delayMicroseconds(TOTAL/4);
+  waitPeriod(TOTAL/4);
   digitalWrite(PA, LOW);
-  delayMicroseconds(TOTAL/4);
+  waitPeriod(TOTAL/4);
   digitalWrite(PB, LOW);
-  delayMicroseconds(TOTAL/4);
+  waitPeriod(TOTAL/4);
 }
 
+void waitPeriod(long usecs)
+{
+  if (usecs > 1000) {
+    delay(usecs/1000);
+  }
+  else {
+    delayMicroseconds(usecs);
+  }
+}
 
