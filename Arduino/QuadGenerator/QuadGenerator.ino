f5aaa39f724a839ebfe7fd2c63ffc6259f673adb
Wilbert Alberts <wilbert.alberts@gmail.com>
2016-07-09 20:06:03 +0200

    Feature: quadrature generator for separate arduino
    	Rationale: by creating a a S0S90 signal, it is possible
    	to debug the decoding on the AVR and stress test it.

---
+++ Arduino/QuadGenerator/QuadGenerator.ino
@@ -0,0 +1,26 @@
+
+#define PA (5)
+#define PB (6)
+
+#define TOTAL (1000)
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  
+  // put your main code here, to run repeatedly:
+  digitalWrite(PA, HIGH);
+  delayMicroseconds(TOTAL/4);
+  digitalWrite(PB, HIGH);
+  delayMicroseconds(TOTAL/4);
+  digitalWrite(PA, LOW);
+  delayMicroseconds(TOTAL/4);
+  digitalWrite(PB, LOW);
+  delayMicroseconds(TOTAL/4);
+}
+
+
