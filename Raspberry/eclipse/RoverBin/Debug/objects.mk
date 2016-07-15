8611f5de2eb6e142e307ac498203b88fa7ddbe3f
Wilbert Alberts <wilbert.alberts@gmail.com>
2016-07-15 20:06:08 +0200

    Fix: again, try to get reasonable build settings.

--- Raspberry/eclipse/RoverBin/Debug/objects.mk
+++ Raspberry/eclipse/RoverBin/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS :=
+LIBS := -lRover -lwiringPi -lpthread
 
--- Raspberry/eclipse/RoverBin/Debug/sources.mk
+++ Raspberry/eclipse/RoverBin/Debug/sources.mk
@@ -6,27 +6,22 @@ C_UPPER_SRCS :=
 CXX_SRCS := 
 C++_SRCS := 
 OBJ_SRCS := 
-INO_SRCS := 
-PDE_SRCS := 
 CC_SRCS := 
 ASM_SRCS := 
-CPP_SRCS := 
 C_SRCS := 
-S_UPPER_SRCS := 
+CPP_SRCS := 
 O_SRCS := 
+S_UPPER_SRCS := 
 CC_DEPS := 
 C++_DEPS := 
 EXECUTABLES := 
 OBJS := 
 C_UPPER_DEPS := 
 CXX_DEPS := 
-INO_DEPS := 
-PDE_DEPS := 
-CPP_DEPS := 
 C_DEPS := 
+CPP_DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-DebugSim/src \
 src \
 
