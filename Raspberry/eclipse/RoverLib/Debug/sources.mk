413c79d9fed85dd66fe1bcaf827c470822c28384
Wilbert Alberts <wilbert.alberts@gmail.com>
2016-07-15 19:51:06 +0200

    Fix: resture default build settings.

--- Raspberry/eclipse/RoverLib/Debug/sources.mk
+++ Raspberry/eclipse/RoverLib/Debug/sources.mk
@@ -5,20 +5,19 @@
 C_UPPER_SRCS := 
 CXX_SRCS := 
 C++_SRCS := 
-INO_SRCS := 
-PDE_SRCS := 
+OBJ_SRCS := 
 CC_SRCS := 
 ASM_SRCS := 
 C_SRCS := 
 CPP_SRCS := 
+O_SRCS := 
 S_UPPER_SRCS := 
 CC_DEPS := 
 C++_DEPS := 
 OBJS := 
 C_UPPER_DEPS := 
 CXX_DEPS := 
-INO_DEPS := 
-PDE_DEPS := 
+ARCHIVES := 
 C_DEPS := 
 CPP_DEPS := 
 
--- Raspberry/eclipse/RoverLib/Debug/src/subdir.mk
+++ Raspberry/eclipse/RoverLib/Debug/src/subdir.mk
@@ -8,28 +8,31 @@ CPP_SRCS += \
 ../src/rv_exchange.cpp \
 ../src/rv_log.cpp \
 ../src/rv_loop.cpp \
-../src/rv_reg.cpp 
+../src/rv_reg.cpp \
+../src/rv_trace.cpp 
 
 OBJS += \
 ./src/rv.o \
 ./src/rv_exchange.o \
 ./src/rv_log.o \
 ./src/rv_loop.o \
-./src/rv_reg.o 
+./src/rv_reg.o \
+./src/rv_trace.o 
 
 CPP_DEPS += \
 ./src/rv.d \
 ./src/rv_exchange.d \
 ./src/rv_log.d \
 ./src/rv_loop.d \
-./src/rv_reg.d 
+./src/rv_reg.d \
+./src/rv_trace.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	arm-linux-gnueabihf-g++ -I../../wiringPi/src -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
