e13ccfcd2ee29841866b16b05700763a55cc5656
Wilbert Alberts <wilbert.alberts@asml.com>
2016-07-27 13:52:43 +0200

    Fix:

--- Raspberry/eclipse/RoverBin/src/main.cpp
+++ Raspberry/eclipse/RoverBin/src/main.cpp
@@ -20,28 +20,28 @@ int main (int argc, char* argv[])
     long right;
     int sig[4];
     int amb[4];
-    //RV_loggingOn();
-    //RV_loopLoggingOn();
+    RV_loggingOff();
+    RV_loopLoggingOff();
     RV_setFrequency(100);
     RV_start();
     sleep(1);
-    //RV_move(RV_FORWARD, RV_FORWARD, 50, 50);
+    RV_move(RV_BACKWARD, RV_BACKWARD, 70,70);
     for (int i=0; i<10000; i++) {
       RV_getPosition(&left, &right);
       //printf("left: %ld, right: %ld\n", left, right);
       
-      //RV_getAnalogCollision(sig, amb);
+      RV_getAnalogCollision(sig, amb);
       //printf("left: %ld, right: %ld, NE: %d, SE: %d, SW: %d, NW: %d\n", 
-      //	     left, right, sig[0], sig[1], sig[2], sig[3]);
+      // 	     left, right, sig[0], sig[1], sig[2], sig[3]);
+      
       //if (mustStop(sig)) {
       //  RV_move(RV_FORWARD, RV_FORWARD, 0, 0);
       //  i=100000;
       //}      
-      
+
       RV_getAnalogLine(sig, amb);
       printf("left: %ld, right: %ld, N: %d, E: %d, S: %d, W: %d\n", 
       	     left, right, sig[0], sig[1], sig[2], sig[3]);
-      
       
       RV_waitForNewData();
     }
